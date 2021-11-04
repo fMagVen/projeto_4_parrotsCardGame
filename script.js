2ef05756cd496d6c3b06e91b9496ca71644d06ef
fMagVen <78576546+fMagVen@users.noreply.github.com>
2021-11-04 09:28:49 -0300

    added card insertion

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-let amountOfCards = prompt("How many cards? Choose an even number between 2 and 14");
+let amountOfCards = prompt("How many cards? Choose an even number between 4 and 14");
 
-while(amountOfCards % 2 != 0 || amountOfCards <2 || amountOfCards > 14)
+while(amountOfCards % 2 != 0 || amountOfCards < 4 || amountOfCards > 14)
 {
-    if(amountOfCards < 2 || amountOfCards > 14)
-        amountOfCards = prompt("You must choose a number between 2 and 14! The number must also be even.");
+    if(amountOfCards < 4 || amountOfCards > 14)
+        amountOfCards = prompt("You must choose a number between 4 and 14! The number must also be even.");
 
     if(amountOfCards % 2 != 0)
         amountOfCards = prompt("You must choose an even number! The number must also be between 2 and 14.")
+}
+
+let cardSpace = document.querySelector(".game");
+
+for(let i = 2; i <= amountOfCards; i = i + 2)
+{
+    cardSpace.innerHTML = cardSpace.innerHTML + `
+    <div class="pair flex">
+            <div class="card flex" data-identifier="card">
+                <div class="front" data-identifier="front-face">
+                    <img class="front-parrot" src="/assets/front.png" alt="https://www.youtube.com/watch?v=nYc09Xqy3hE">
+                </div>
+                <div class="back" data-identifier="back-face">
+
+                </div>
+            </div>
+            <div class="card flex" data-identifier="card">
+                <div class="front" data-identifier="front-face">
+                    <img class="front-parrot" src="/assets/front.png" alt="https://www.youtube.com/watch?v=nYc09Xqy3hE">
+                </div>
+                <div class="back" data-identifier="back-face">
+
+                </div>
+
+            </div>
+        </div>
+    `
 }
