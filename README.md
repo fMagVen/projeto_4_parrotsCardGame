1edaa8a240e67c7b61c3baf00d027a6cb9bb82da
fMagVen <78576546+fMagVen@users.noreply.github.com>
2021-11-07 00:32:10 -0300

    game implemented

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Also using it to learn more about and to practice git and github!
 
 CHANGELOG:
 
+known issues: game breaking bug when multiple cards are selected one after the other in a short time
+
+0.5 - game itself implemented! test your memory choosing from 4 to 14 cards and trying to match the funny parrots in the smallest number of clicks possible!
+      your score will be shown in an alert pop-up when you finish!
+
 0.4 - added final front card design
       different pair of parrot gifs will be randomly given to all cards in the game
 
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let amountOfCards = prompt("How many cards? Choose an even number between 4 and 14");
 
-while(amountOfCards % 2 != 0 || amountOfCards < 4 || amountOfCards > 14)
-{
-    if(amountOfCards < 4 || amountOfCards > 14)
+while (amountOfCards % 2 != 0 || amountOfCards < 4 || amountOfCards > 14) {
+    if (amountOfCards < 4 || amountOfCards > 14)
         amountOfCards = prompt("You must choose a number between 4 and 14! The number must also be even.");
 
-    if(amountOfCards % 2 != 0)
+    if (amountOfCards % 2 != 0)
         amountOfCards = prompt("You must choose an even number! The number must also be between 2 and 14.");
 }
 
@@ -13,21 +12,22 @@ let backParrots = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparro
 backParrots.sort(comparator);
 
 let parrotsInTheGame = [];
-for (let i = 0; i < amountOfCards/2; i++)
-{
+for (let i = 0; i < amountOfCards / 2; i++) {
     parrotsInTheGame.push(backParrots[i]);
     parrotsInTheGame.push(backParrots[i]);
 }
 parrotsInTheGame.sort(comparator);
 
 let parrotPlacer;
+let turnMoves = 0;
+let selectedCards;
+let numberOfTurnedCards;
 
 let cardSpace = document.querySelector(".game");
-for(let i = 0; i < amountOfCards/2; i++)
-{
+for (let i = 0; i < amountOfCards / 2; i++) {
     cardSpace.innerHTML = cardSpace.innerHTML + `
     <div class="pair flex">
-            <div class="card flex" data-identifier="card" onclick=turn(this)>
+        <div class="card flex" data-identifier="card" onclick=turn(this)>
             <div class="front side flex" data-identifier="front-face">
                 <img class="front-parrot" src="/assets/front.png" alt="vector drawing of a parrot facing left">
             </div>
@@ -35,10 +35,10 @@ for(let i = 0; i < amountOfCards/2; i++)
                 <img class="back-parrot" src="/assets/${placeParrot()}.gif" alt="gif of a parrot">
             </div>
         </div>
-            <div class="card flex" data-identifier="card" onclick=turn(this)>
+        <div class="card flex" data-identifier="card" onclick=turn(this)>
             <div class="front side flex" data-identifier="front-face">
                 <img class="front-parrot" src="/assets/front.png" alt="vector drawing of a parrot facing left">
-            </div>
+           </div>
             <div class="back side flex" data-identifier="back-face">
                 <img class="back-parrot" src="/assets/${placeParrot()}.gif" alt="gif of a parrot">
             </div>
@@ -47,23 +47,73 @@ for(let i = 0; i < amountOfCards/2; i++)
     `
 }
 
-function comparator()
-{
+function comparator() {
     return Math.random() - 0.5;
+}
+function placeParrot() {
+    parrotPlacer = parrotsInTheGame[parrotsInTheGame.length - 1];
+    parrotsInTheGame.pop();
+    return parrotPlacer;
 }
 
 
-function turn(cardToTurn)
-{
+function turn(cardToTurn) {
+
+    let tempDisable = document.querySelectorAll(".card");
+    for(let i = 0; i < amountOfCards; i++)
+    {
+        tempDisable[i];
+    }
+
     let turningTheCard = cardToTurn.querySelector(".front");
     turningTheCard.classList.add("front-turn");
     turningTheCard = cardToTurn.querySelector(".back");
     turningTheCard.classList.add("back-turn");
+    cardToTurn.classList.add("recently-turned");
+    turnMoves++;
+    gameFlow();
+}
+function gameFlow() {
+    selectedCards = document.querySelectorAll(".card.recently-turned");
+    console.log(selectedCards);
+    if(selectedCards.length == 2)
+    {
+        let firstCard = selectedCards[0].querySelector(".back-parrot").getAttribute('src');
+        let secondCard = selectedCards[1].querySelector(".back-parrot").getAttribute('src');
+        if(firstCard !== secondCard)
+        {
+            setTimeout(turnBack, 2000);
+        }
+        else
+        {
+            selectedCards[0].classList.remove("recently-turned");
+            selectedCards[0].classList.add("previously-turned");
+            selectedCards[1].classList.remove("recently-turned");
+            selectedCards[1].classList.add("previously-turned");
+        }
+    }
+    numberOfTurnedCards = document.querySelectorAll(".card.previously-turned");
+    if(numberOfTurnedCards.length == amountOfCards){
+        setTimeout(endGame, 500);
+    }
 }
 
-function placeParrot()
+function turnBack()
 {
-    parrotPlacer = parrotsInTheGame[parrotsInTheGame.length - 1];
-    parrotsInTheGame.pop();
-    return parrotPlacer;
+    let turnTheseBack = document.querySelectorAll(".recently-turned");
+    let turningBack = turnTheseBack[0].querySelector(".front");
+    turningBack.classList.remove("front-turn");
+    turningBack = turnTheseBack[0].querySelector(".back");
+    turningBack.classList.remove("back-turn");
+    turnTheseBack[0].classList.remove("recently-turned");
+    turningBack = turnTheseBack[1].querySelector(".front");
+    turningBack.classList.remove("front-turn");
+    turningBack = turnTheseBack[1].querySelector(".back");
+    turningBack.classList.remove("back-turn");
+    turnTheseBack[1].classList.remove("recently-turned");
+}
+
+function endGame()
+{
+    alert(`Você terminou em ${turnMoves} jogadas!`);
 }
